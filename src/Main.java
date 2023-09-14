4c56d122f9fc0958310bb01ca9ab3b43674ebed6
Saadvikk <saadvik@outlook.com>
2023-09-14 16:11:10 -0400

    first lab part 2 question 2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        int fallCost = 1000;
+        int winterCost = 2000;
+        int springCost = 3000;
+        int summerCost = 4000;
+        int homeMaintenance = fallCost + winterCost + springCost + summerCost;
+
+        System.out.println("Your annual home maintenance cost is " + homeMaintenance + " dollars.");
     }
 }
